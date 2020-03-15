f61ede42113e223f0f5a19a29fc0b236eaf8186a
aoneko25 <aoneko820@yahoo.co.jp>
2020-03-16 01:53:06 +0900

    マークアップリンク作業
    マイページ、プロフィール、クレジット、本人情報確認ページ、商品購入確認ページにコントローラー作成
    コントローラーにdef index endアクション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   root "posts#index"
   resources :sellcontents, only: [:index]
+  resources :check, only: [:index]
+  resources :profile, only: [:index]
+  resources :mypage, only: [:index]
+  resources :credit, only: [:index]
+  resources :confirm, only: [:index]
 end
