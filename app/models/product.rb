bccaf9ccc93d49609c4deea4ff8b79c90e22ace4
delfin378 <type0bs02hd@yahoo.co.jp>
2020-04-15 10:38:40 +0900

    配列

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ApplicationRecord
   has_many :comments
   has_many :images, dependent: :destroy
-  # belongs_to :user
-  enum trade_status:{selling: 0, negotiating: 1, freeze: 2}
+  enum trade_status:[ :selling, :negotiating, :freeze]
   accepts_nested_attributes_for :images, allow_destroy: true
 end
