d3b81d76cd9c40caed42668ecbf2c11dc67d4bf2
aoneko25 <aoneko820@yahoo.co.jp>
2020-04-30 17:15:29 +0900

    エラー解消、画面修正

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,9 @@ class RegistrationsController < ApplicationController
     session[:first_name] = user_params[:first_name]
     session[:family_name_kana] = user_params[:family_name_kana]
     session[:first_name_kana] = user_params[:first_name_kana]
-    session[:birthday] = user_params[:birthday]
+    session["birthday(1i)"] = params["birthday(1i)"]
+    session["birthday(2i)"] = params["birthday(2i)"]
+    session["birthday(3i)"] = params["birthday(3i)"]
     @user = User.new
   end
 
@@ -22,10 +24,6 @@ class RegistrationsController < ApplicationController
   end
 
   def new_credit
-    session[:family_name] = address_params[:family_name]
-    session[:first_name] = address_params[:first_name]
-    session[:family_name_kana] = address_params[:family_name_kana]
-    session[:first_name_kana] = address_params[:first_name_kana]
     session[:postal_code] = address_params[:postal_code]
     session[:prefectures] = address_params[:prefectures]
     session[:municipalities] = address_params[:municipalities]
@@ -38,16 +36,19 @@ class RegistrationsController < ApplicationController
 
   def complete
     session[:card_number] = card_params[:card_number]
-    session[:expiration_date] = card_params[:expiration_date]
+    session["expiration_date(1i)"] = params["expiration_date(1i)"]
+    session["expiration_date(2i)"] = params["expiration_date(2i)"]
     session[:securitycord] = card_params[:securitycord]
     @user = User.create(nickname: session[:nickname], email: session[:email], password: session[:password], family_name: session[:family_name], first_name: session[:first_name], family_name_kana: session[:family_name_kana], first_name_kana: session[:first_name_kana], birthday: session[:birthday], phone_number: session[:phone_number])
-    @address = Address.create(family_name: session[:family_name], first_name: session[:first_name], family_name_kana: session[:family_name_kana], first_name_kana: session[:first_name_kana], postal_code: session[:postal_code], prefectures: session[:prefectures], municipalities: session[:municipalities], house_number: session[:house_number], building_name: session[:building_name], house_phone_number: session[:house_phone_number])
+    @address = Address.create(postal_code: session[:postal_code], prefectures: session[:prefectures], municipalities: session[:municipalities], house_number: session[:house_number], building_name: session[:building_name], house_phone_number: session[:house_phone_number])
     @card = Card.create(card_number: session[:card_number], expiration_date: session[:expiration_date], securitycord: session[:securitycord])
-    params[:birthday] = birthday_join
     if @user.save
        @address.save
        @card.save
+       @user.update(birthday: birthday_join)
       session[:id] = @user.id
+      session[:id] = @address.id
+      session[:id] = @card.id
     end
   end
 
@@ -67,21 +68,10 @@ class RegistrationsController < ApplicationController
       :profile,
       :icon
     )
-  end
-  def birthday_join
-    date = params[:user][:birthday]
-    Date.new date["birthday(1i)"].to_i,date["birthday(2i)"].to_i,date["birthday(3i)"].to_i
-  end  
-  def birthday_join
-    date = params[:birthday]
-  end
+  end 
   def address_params
     params.require(:address).permit(
       :user_id,
-      :family_name,
-      :first_name,
-      :family_name_kana,
-      :first_name_kana,
       :postal_code,
       :prefectures,
       :municipalities,
@@ -94,10 +84,13 @@ class RegistrationsController < ApplicationController
     params.require(:card).permit(
       :user_id,
       :card_number,
-      :expiration_month,
-      :expiration_year,
+      :expiration_date,
       :securitycord,
     )
     end
+    def expiration_date_join
+      date = params[:expiration_date]
+      Date.new date[expiration_date(1i)].to_i,date[expiration_date(2i)].to_i
+    end  
   end
 
--- db/migrate/20200423104852_create_cards.rb
+++ db/migrate/20200423104852_create_cards.rb
@@ -1,9 +1,9 @@
 class CreateCards < ActiveRecord::Migration[5.0]
   def change
     create_table :cards do |t|
-      t.integer :user_id, null: false, foreign_key: true
+      t.integer :user_id, optional: true
       t.integer :card_number, null: false, foreign_key: true
-      t.integer :expiration_date, null: false
+      t.date :expiration_date, null: false
       t.integer :securitycord, null: false
 
       t.timestamps
