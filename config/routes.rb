935fe7ad9159ee385d19c9205547f0dd7e6f65b6
delfin378 <type0bs02hd@yahoo.co.jp>
2020-05-17 11:30:40 +0900

    トップページビュー修正

--- config/routes.rb
+++ config/routes.rb
@@ -27,4 +27,5 @@ Rails.application.routes.draw do
   resources :confirm, only: [:index]
   resources :productiondetail, only: [:index]
   resources :logout, only: [:index]
+  resources :login, only: [:index]
 end
