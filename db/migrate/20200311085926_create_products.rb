9d55bd4c74251a570a5d433aaae68f925a441544
delfin378 <type0bs02hd@yahoo.co.jp>
2020-05-21 17:24:36 +0900

    Revert "DB修正（images,products）"
    This reverts commit 6725a1a1410278f4e9d771a71baa5d24e524012a.

--- db/migrate/20200311085926_create_products.rb
+++ db/migrate/20200311085926_create_products.rb
@@ -12,7 +12,6 @@ class CreateProducts < ActiveRecord::Migration[5.0]
       t.integer  :shipping_origin    , null: false
       t.integer  :arrival_days       , null: false
       t.references :user         , optional: true
-
       t.timestamps
     end
   end
